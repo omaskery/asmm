7f41aa34a3c8b7d440b049b45f241524ba94d201
Oliver Maskery <omaskery@googlemail.com>
2019-04-28 20:45:06 +0100

    added missing docstrings to deps.py

--- asmm/deps.py
+++ asmm/deps.py
@@ -1,3 +1,5 @@
+"""Manages the dependencies of a project
+"""
 import os
 
 
@@ -6,6 +8,11 @@ from .exception import AsmmError
 
 
 def add_dependency(target_directory, dependency):
+    """
+    Add a dependency to the project as a URI to another project
+    :param target_directory: directory of the project to modify with new dependency
+    :param dependency: URI of project to treat as a dependency
+    """
     project_file_path = os.path.join(target_directory, ".asmm/config.yml")
     project = load_project(project_file_path)
     if dependency in project.dependencies:
@@ -15,6 +22,11 @@ def add_dependency(target_directory, dependency):
 
 
 def list_dependencies(target_directory):
+    """
+    Lists the dependencies of the current project
+    :param target_directory: path to the directory of the current project
+    :return: a list of dependency URIs
+    """
     project_file_path = os.path.join(target_directory, ".asmm/config.yml")
     project = load_project(project_file_path)
     return project.dependencies
